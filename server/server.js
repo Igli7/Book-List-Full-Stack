821a62d15c2622bd0a925eb96a6cbc7341cbd19a
Igli Xhukellari <xhukellariigli@gmail.com>
2020-11-07 20:21:09 -0500

    Deployment

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const connectDB = require('./config/db');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+var http = require('http');
 
 // Connect DB
 connectDB();
@@ -24,6 +25,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 const PORT = process.env.PORT || 3500;
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 module.exports = app;
